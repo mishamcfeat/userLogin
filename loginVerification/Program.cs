d18063587351cdc38430e0eb351450b1412fcf0c
Misha McFeat <misha.mcfeat@gmail.com>
2024-03-02 23:12:08 +0000

    Initial code with functional login

---
+++ loginVerification/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace loginVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Enter username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    Console.WriteLine("Please Enter Username and Password!");
+                    continue;
+                }
+
+                if (username.ToLower() == "admin" && password == "PassWord123!")
+                {
+                    Console.WriteLine("Login successful!");
+                    break;
+                }
+            } while (true);
+
+
+
+        }
+    }
+}
